7aeaa511274dd67072585b534e9a09de18afc8bb
Alexandre Devert <alex_devert@adagene.cn>
2019-01-14 16:31:55 +0100

    Added testing of optimality

--- test_miniball.py
+++ test_miniball.py
@@ -28,8 +28,38 @@ def test_bounding_ball_contains_point_set():
 	# Check that the computed bounding ball contains all the input points
 	for n in range(1, 10):
 		for count in range(2, n + 10):
+			# Generate points
 			S = numpy.random.randn(count, n)
+
+			# Get the bounding sphere
 			C, r = miniball.get_bounding_ball(S)
 
+			# Check that all points are inside the bounding sphere up to machine precision
 			assert numpy.all(numpy.sum((S - C) ** 2, axis = 1) - r ** 2 < 1e-12)
+
+
+
+def test_bounding_ball_optimality():
+	# Check that the bounding ball are optimal
+	for n in range(2, 10):
+		for count in range(n + 2, n + 30):
+			# Generate a support sphere from n+1 points
+			S_support = numpy.random.randn(n + 1, n)
+			C_support, r_support = miniball.get_bounding_ball(S_support)
+			
+			# Generate points inside the support sphere
+			S = numpy.random.randn(count - S_support.shape[0], n)
+			S /= numpy.sqrt(numpy.sum(S ** 2, axis = 1))[:,None]
+			S *= ((.5 * r_support) * numpy.random.rand(count - S_support.shape[0], 1))
+			S = S + C_support
+				
+			# Get the bounding sphere
+			C, r = miniball.get_bounding_ball(numpy.concatenate([S, S_support], axis = 0))
+
+			# Check that the bounding sphere and the support sphere are equivalent
+			# up to machine precision.
+			print count, r, r_support
+			assert numpy.allclose(r, r_support)
+			assert numpy.allclose(C, C_support)
+
 
