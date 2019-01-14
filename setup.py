114c02cbd5ca70c2633eea875c8dcd8cec4f3e39
Alexandre Devert <alex_devert@adagene.cn>
2019-01-14 15:03:59 +0100

    Fixed wrong link to README file setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2019 Alexandre Devert
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
+# of this software and associated documentation files (the 'Software'), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
@@ -11,7 +11,7 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -26,29 +26,30 @@ import miniball
 
 
 miniball_classifiers = [
-	"Development Status :: 5 - Production/Stable",
-	"Programming Language :: Python :: 2",
-	"Programming Language :: Python :: 3",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Topic :: Software Development :: Libraries",
-	"Topic :: Utilities",
+	'Development Status :: 5 - Production/Stable',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 3',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: MIT License',
+	'Topic :: Software Development :: Libraries',
+	'Topic :: Utilities',
 ]
 
-with open("README.rst", "r") as f:
+with open('README.md', 'r') as f:
 	miniball_long_description = f.read()
 
 setup(
-	name = "miniball",
+	name = 'miniball',
 	version = miniball.__version__,
-	author = "Alexandre Devert",
-	author_email = "marmakoide@hotmail.fr",
-	url = "https://github.com/marmakoide/miniball",
-	tests_require = ["pytest"],
-	py_modules = ["miniball"],
-	description = "Efficiently computes the smallest bounding ball of a point set, in arbitrary number of dimensions.",
+	author = 'Alexandre Devert',
+	author_email = 'marmakoide@hotmail.fr',
+	url = 'https://github.com/marmakoide/miniball',
+	install_requires = ['numpy'],
+	tests_require = ['pytest'],
+	py_modules = ['miniball'],
+	description = 'Efficiently computes the smallest bounding ball of a point set, in arbitrary number of dimensions.',
 	long_description = miniball_long_description,
-	license = "MIT",
+	license = 'MIT',
 	classifiers = miniball_classifiers,
-	python_requires = ">=2.7, !=3.0.*, !=3.1.*",
+	python_requires = '>=2.7, !=3.0.*, !=3.1.*',
 )
