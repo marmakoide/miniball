14498b86b77c9503ed81e27852c24342c0cb51aa
Alexandre Devert <marmakoide@hotmail.fr>
2021-04-28 14:26:48 +0200

    All python files are formatted with black

--- miniball.py
+++ miniball.py
@@ -23,106 +23,103 @@ import numpy
 import random
 
 
-
-__author__  = 'Alexandre Devert <marmakoide@hotmail.fr>'
-__version__ = '1.0.4'
-
+__author__ = "Alexandre Devert <marmakoide@hotmail.fr>"
+__version__ = "1.0.4"
 
 
 def get_circumsphere(S):
-	"""
-	Computes the circumsphere of a set of points
+    """
+    Computes the circumsphere of a set of points
 
-	Parameters
-	----------
-	S : (M, N) ndarray, where 1 <= M <= N + 1
-		The input points
+    Parameters
+    ----------
+    S : (M, N) ndarray, where 1 <= M <= N + 1
+            The input points
 
-	Returns
-	-------
-	C, r2 : ((2) ndarray, float)
-		The center and the squared radius of the circumsphere 
-	"""
+    Returns
+    -------
+    C, r2 : ((2) ndarray, float)
+            The center and the squared radius of the circumsphere
+    """
 
-	U = S[1:] - S[0] 
-	B = numpy.sqrt(numpy.sum(U ** 2, axis = 1))
-	U /= B[:,None]
-	B /= 2
-	C = numpy.dot(numpy.linalg.solve(numpy.inner(U, U), B), U)
-	r2 = numpy.sum(C ** 2)
-	C += S[0]
-	return C, r2
+    U = S[1:] - S[0]
+    B = numpy.sqrt(numpy.sum(U ** 2, axis=1))
+    U /= B[:, None]
+    B /= 2
+    C = numpy.dot(numpy.linalg.solve(numpy.inner(U, U), B), U)
+    r2 = numpy.sum(C ** 2)
+    C += S[0]
+    return C, r2
 
 
+def get_bounding_ball(S, epsilon=1e-7):
+    """
+    Computes the smallest bounding ball of a set of points
 
-def get_bounding_ball(S, epsilon = 1e-7):
-	"""
-	Computes the smallest bounding ball of a set of points
+    Parameters
+    ----------
+    S : (M, N) ndarray, where 1 <= M <= N + 1
+            The input points
 
-	Parameters
-	----------
-	S : (M, N) ndarray, where 1 <= M <= N + 1
-		The input points
+    epsilon : float
+            Tolerance used when testing if a set of point belongs to the same sphere.
+            Default is 1e-7
 
-	epsilon : float
-		Tolerance used when testing if a set of point belongs to the same sphere.
-		Default is 1e-7
+    Returns
+    -------
+    C, r2 : ((2) ndarray, float)
+            The center and the squared radius of the circumsphere
+    """
 
-	Returns
-	-------
-	C, r2 : ((2) ndarray, float)
-		The center and the squared radius of the circumsphere 
-	"""
+    # Iterative implementation of Welzl's algorithm, see
+    # "Smallest enclosing disks (balls and ellipsoids)" Emo Welzl 1991
 
-	# Iterative implementation of Welzl's algorithm, see
-	# "Smallest enclosing disks (balls and ellipsoids)" Emo Welzl 1991
+    def circle_contains(D, p):
+        c, r2 = D
+        return numpy.sum((p - c) ** 2) <= r2
 
-	def circle_contains(D, p):
-		c, r2 = D
-		return numpy.sum((p - c) ** 2) <= r2
+    def get_boundary(R):
+        if len(R) == 0:
+            return numpy.zeros(S.shape[1]), 0.0
 
-	def get_boundary(R):
-		if len(R) == 0:
-			return numpy.zeros(S.shape[1]), 0.
+        if len(R) <= S.shape[1] + 1:
+            return get_circumsphere(S[R])
 
-		if len(R) <= S.shape[1] + 1:
-			return get_circumsphere(S[R])
+        c, r2 = get_circumsphere(S[R[: S.shape[1] + 1]])
+        if numpy.all(numpy.fabs(numpy.sum((S[R] - c) ** 2, axis=1) - r2) < epsilon):
+            return c, r2
 
-		c, r2 = get_circumsphere(S[R[:S.shape[1] + 1]])
-		if numpy.all(numpy.fabs(numpy.sum((S[R] - c) ** 2, axis = 1) - r2) < epsilon):
-				return c, r2
+    class Node(object):
+        def __init__(self, P, R):
+            self.P = P
+            self.R = R
+            self.D = None
+            self.pivot = None
+            self.left = None
+            self.right = None
 
-	class Node(object):
-		def __init__(self, P, R):
-			self.P = P
-			self.R = R
-			self.D = None
-			self.pivot = None
-			self.left = None
-			self.right = None
+    def traverse(node):
+        stack = [node]
+        while len(stack) > 0:
+            node = stack.pop()
 
-	def traverse(node):
-		stack = [node]
-		while len(stack) > 0:
-			node = stack.pop()
+            if len(node.P) == 0 or len(node.R) >= S.shape[1] + 1:
+                node.D = get_boundary(node.R)
+            elif node.left is None:
+                node.pivot = random.choice(node.P)
+                node.left = Node(list(set(node.P) - set([node.pivot])), node.R)
+                stack.extend((node, node.left))
+            elif node.right is None:
+                if circle_contains(node.left.D, S[node.pivot]):
+                    node.D = node.left.D
+                else:
+                    node.right = Node(node.left.P, node.R + [node.pivot])
+                    stack.extend((node, node.right))
+            else:
+                node.D = node.right.D
+                node.left, node.right = None, None
 
-			if len(node.P) == 0 or len(node.R) >= S.shape[1] + 1:
-				node.D = get_boundary(node.R)
-			elif node.left is None:
-				node.pivot = random.choice(node.P)			 
-				node.left = Node(list(set(node.P) - set([node.pivot])), node.R) 		
-				stack.extend((node, node.left))
-			elif node.right is None:
-				if circle_contains(node.left.D, S[node.pivot]):
-					node.D = node.left.D
-				else:
-					node.right = Node(node.left.P, node.R + [node.pivot])
-					stack.extend((node, node.right))
-			else:
-				node.D = node.right.D
-				node.left, node.right = None, None
-
-	S = S.astype(float, copy = False)
-	root = Node(range(S.shape[0]), [])
-	traverse(root)	
-	return root.D
+    S = S.astype(float, copy=False)
+    root = Node(range(S.shape[0]), [])
+    traverse(root)
+    return root.D
--- test_miniball.py
+++ test_miniball.py
@@ -23,54 +23,55 @@ import numpy
 import miniball
 
 
-
 def test_integer_coordinates():
-	# Check that integer coordinates are properly handled
-	for n in range(1, 10):
-		S_int = numpy.random.randint(-1000, 1500, (100, 2))
-		C_int, r2_int = miniball.get_bounding_ball(S_int)
+    # Check that integer coordinates are properly handled
+    for n in range(1, 10):
+        S_int = numpy.random.randint(-1000, 1500, (100, 2))
+        C_int, r2_int = miniball.get_bounding_ball(S_int)
 
-		S = S_int.astype(float)
-		C, r2 = miniball.get_bounding_ball(S)
+        S = S_int.astype(float)
+        C, r2 = miniball.get_bounding_ball(S)
 
-		assert numpy.allclose(C, C_int)
-		assert numpy.allclose(r2, r2_int)
-
+        assert numpy.allclose(C, C_int)
+        assert numpy.allclose(r2, r2_int)
 
 
 def test_bounding_ball_contains_point_set():
-	# Check that the computed bounding ball contains all the input points
-	for n in range(1, 10):
-		for count in range(2, n + 10):
-			# Generate points
-			S = numpy.random.randn(count, n)
+    # Check that the computed bounding ball contains all the input points
+    for n in range(1, 10):
+        for count in range(2, n + 10):
+            # Generate points
+            S = numpy.random.randn(count, n)
 
-			# Get the bounding sphere
-			C, r2 = miniball.get_bounding_ball(S)
+            # Get the bounding sphere
+            C, r2 = miniball.get_bounding_ball(S)
 
-			# Check that all points are inside the bounding sphere up to machine precision
-			assert numpy.all(numpy.sum((S - C) ** 2, axis = 1) - r2 < 1e-12)
-
+            # Check that all points are inside the bounding sphere up to machine precision
+            assert numpy.all(numpy.sum((S - C) ** 2, axis=1) - r2 < 1e-12)
 
 
 def test_bounding_ball_optimality():
-	# Check that the bounding ball are optimal
-	for n in range(2, 10):
-		for count in range(n + 2, n + 30):
-			# Generate a support sphere from n+1 points
-			S_support = numpy.random.randn(n + 1, n)
-			C_support, r2_support = miniball.get_bounding_ball(S_support)
-			
-			# Generate points inside the support sphere
-			S = numpy.random.randn(count - S_support.shape[0], n)
-			S /= numpy.sqrt(numpy.sum(S ** 2, axis = 1))[:,None]
-			S *= ((.9 * numpy.sqrt(r2_support)) * numpy.random.rand(count - S_support.shape[0], 1))
-			S = S + C_support
-				
-			# Get the bounding sphere
-			C, r2 = miniball.get_bounding_ball(numpy.concatenate([S, S_support], axis = 0))
+    # Check that the bounding ball are optimal
+    for n in range(2, 10):
+        for count in range(n + 2, n + 30):
+            # Generate a support sphere from n+1 points
+            S_support = numpy.random.randn(n + 1, n)
+            C_support, r2_support = miniball.get_bounding_ball(S_support)
 
-			# Check that the bounding sphere and the support sphere are equivalent
-			# up to machine precision.
-			assert numpy.allclose(r2, r2_support)
-			assert numpy.allclose(C, C_support)
+            # Generate points inside the support sphere
+            S = numpy.random.randn(count - S_support.shape[0], n)
+            S /= numpy.sqrt(numpy.sum(S ** 2, axis=1))[:, None]
+            S *= (0.9 * numpy.sqrt(r2_support)) * numpy.random.rand(
+                count - S_support.shape[0], 1
+            )
+            S = S + C_support
+
+            # Get the bounding sphere
+            C, r2 = miniball.get_bounding_ball(
+                numpy.concatenate([S, S_support], axis=0)
+            )
+
+            # Check that the bounding sphere and the support sphere are equivalent
+            # up to machine precision.
+            assert numpy.allclose(r2, r2_support)
+            assert numpy.allclose(C, C_support)
